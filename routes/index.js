001f754dfd323cdf750dcd34dd0bb714d364d774
Tony Bogun <antonybogun@gmail.com>
2017-02-10 21:55:22 -0500

    Update about page, add icons, this closes #3

--- routes/index.js
+++ routes/index.js
@@ -3,42 +3,43 @@ let router = express.Router();
 
 // Global Route Variables
 let currentDate = new Date();
-  currentDate = currentDate.toLocaleTimeString();
+currentDate = currentDate.toLocaleTimeString();
 
 /* GET home page. wildcard */
 router.get('/', (req, res, next) => {
-  res.render('content/index', {
-    title: 'Home',
-    skills:"Web Developer &#x25CF; .NET Developer &#x25CF; Software Engineer"
-   });
+    res.render('content/index', {
+        title: 'Home',
+        skills: "Web Developer &#x25CF; .NET Developer &#x25CF; Software Engineer",
+        description: "I am a responsible, proactive, and fast-learning IT professional with over 2 years’ hands-on experience and strong educational background in Software Engineering, who is passionate about ambitious projects, robust development, and high-quality software"
+    });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('content/about', {
-    title: 'About'
-   });
+    res.render('content/about', {
+        title: 'About'
+    });
 });
 
 /* GET projects page. */
 router.get('/projects', (req, res, next) => {
-  res.render('content/projects', {
-    title: 'Projects'
-   });
+    res.render('content/projects', {
+        title: 'Projects'
+    });
 });
 
 /* GET services page. */
 router.get('/services', (req, res, next) => {
-  res.render('content/services', {
-    title: 'Services'
-   });
+    res.render('content/services', {
+        title: 'Services'
+    });
 });
 
 /* GET contact page. */
 router.get('/contact', (req, res, next) => {
-  res.render('content/contact', {
-    title: 'Contact'
-   });
+    res.render('content/contact', {
+        title: 'Contact'
+    });
 });
 
 module.exports = router;
